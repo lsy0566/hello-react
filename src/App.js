cab63be1ccd841cf29efe88b6a95c7206e25f4ff
dong wook Lee <ehdrnr12@nate.com>
2022-04-26 21:50:07 +0900

    0426

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
 
+// 랜덤 색상 정하기
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 // function App() {
 class App extends Component {
+
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSample/>
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+      <LifeCycleSample color={this.state.color}/>
+      </div>
     );
   }
 }
